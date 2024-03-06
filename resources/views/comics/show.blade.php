e6008707af9594db57a63fea682d240791ff4051
PaoloTarcisio <paoloc50@gmail.com>
2024-03-06 18:47:59 +0100

    aggiunta lista di autori e scrittori in show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -15,6 +15,28 @@
                 <div class="card-body">
                     <h5 class="card-title">Series: {{ $comic->series }}</h5>
                     <p class="card-text">{{ $comic->description }}</p>
+                    <ul>
+                        <li>
+                            Artists:
+                            <ul>
+                                @foreach (json_decode($comic->artists, true) as $artist)
+                                    <li>
+                                        {{ $artist }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                        <li>
+                            Writers:
+                            <ul>
+                                @foreach (json_decode($comic->writers, true) as $writer)
+                                    <li>
+                                        {{ $writer }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    </ul>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
